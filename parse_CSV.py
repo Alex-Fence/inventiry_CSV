8f31386def91204f4c95f7ee26f570c5406cff41
Alex-Fence <bokuto@mail.ru>
2024-10-21 17:24:53 +0300

    получил словарь характеристик

--- parse_CSV.py
+++ parse_CSV.py
@@ -1,6 +1,14 @@
 import csv
-
+hw_dict = {}
 with open('zamfin.csv', 'r', newline = '', encoding = 'utf-8') as csv_file:
     csv_r = csv.reader(csv_file, delimiter='\t')
     for s in csv_r:
-        print(s)
+        if len(s) == 1:
+            name_s = s[0]
+            hw_dict[name_s] = []
+        else:
+            for i in s:
+                if len(i) > 0:
+                    hw_dict[name_s].append(i)
+for k,v in hw_dict.items():
+    print(f'{k}: {v}')
