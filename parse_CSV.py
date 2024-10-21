7268e58456006f894fd8a8f377433b582e549ba9
Alex-Fence <bokuto@mail.ru>
2024-10-21 17:04:02 +0300

    change \t to ''

--- parse_CSV.py
+++ parse_CSV.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('zamfin.csv', 'r', encoding='utf-8') as csv_file:
-    csv_r = csv.reader(csv_file)
+with open('zamfin.csv', 'r', newline = '', encoding = 'utf-8') as csv_file:
+    csv_r = csv.reader(csv_file, delimiter='\t')
     for s in csv_r:
         print(s)
