1e833683173d117097e90716431774ef14193115
Alex-Fence <bokuto@mail.ru>
2024-10-22 22:50:18 +0300

    выделил обработку в отдельную функцию

--- parse_CSV.py
+++ parse_CSV.py
@@ -1,51 +1,52 @@
 import csv
-from os import device_encoding
+from os import listdir
 
-def
-hw_dict = {}
-# загрузка CSV файла
-with open('ZAMFIN.csv', 'r', newline='', encoding='utf-8') as csv_file:
-    csv_r = csv.reader(csv_file, delimiter='\t')
-    # формирование словаря
-    for s in csv_r:
-        if len(s) == 1:
-            name_s = s[0]
-            hw_dict[name_s] = []
-        else:
-            # отсеивание пустых значений
-            for i in s:
-                if len(i) > 0:
-                    hw_dict[name_s].append(i)
-# переделка ключа Материнская плата из-за его искажения при загрузки
-hw_dict['Материнская плата'] = hw_dict.pop('\ufeffМатеринская плата')
-# печать полученного словаря
-# for k, v in hw_dict.items():
-#     print(f'{k}: {v}')
-
-dev_dict_cut = {}
-print()
-# очистка словаря от лишних значений
-for device, options in hw_dict.items():
-    match device:
-        case 'Материнская плата':
-            dev_dict_cut[device] = [options[0]]
-        case 'Процессоры':
-            dev_dict_cut[device] = [options[0]]
-        case 'Оперативная память':
-            dev_dict_cut[device] = [i for i in options if 'Объем' in i or 'Частота' in i]
-        case 'Запоминающие устройства':
-            dev_dict_cut[device] = [i for i in options if not ('Описание' in i or 'Поставщик' in i)]
-        case 'Видеоадаптеры':
-            if len(options) > 5:
-                dev_dict_cut[device] = [options[0], options[5]]
+def get_data_from_file(f_name: str)->dict:
+    hw_dict = {}
+    # загрузка CSV файла
+    with open(f_name, 'r', newline='', encoding='utf-8') as csv_file:
+        csv_r = csv.reader(csv_file, delimiter='\t')
+        # формирование словаря
+        for s in csv_r:
+            if len(s) == 1:
+                name_s = s[0]
+                hw_dict[name_s] = []
             else:
-                dev_dict_cut[device] = [options[0]]
-        case 'Звуковые платы':
-            dev_dict_cut[device] = [options[0]]
-        case 'Адаптеры сетевого интерфейса':
-            dev_dict_cut[device] = [options[0]]
-        case 'Мониторы':
-            dev_dict_cut[device] = [options[1]]
+                # отсеивание пустых значений
+                for i in s:
+                    if len(i) > 0:
+                        hw_dict[name_s].append(i)
+    # переделка ключа Материнская плата из-за его искажения при загрузки
+    hw_dict['Материнская плата'] = hw_dict.pop('\ufeffМатеринская плата')
+    # печать полученного словаря
+    # for k, v in hw_dict.items():
+    #     print(f'{k}: {v}')
 
-for device, options in dev_dict_cut.items():
+    dev_dict_cut = {}
+    # очистка словаря от лишних значений
+    for device, options in hw_dict.items():
+        match device:
+            case 'Материнская плата':
+                dev_dict_cut[device] = [options[0]]
+            case 'Процессоры':
+                dev_dict_cut[device] = [options[0]]
+            case 'Оперативная память':
+                dev_dict_cut[device] = [i for i in options if 'Объем' in i or 'Частота' in i]
+            case 'Запоминающие устройства':
+                dev_dict_cut[device] = [i for i in options if not ('Описание' in i or 'Поставщик' in i)]
+            case 'Видеоадаптеры':
+                if len(options) > 5:
+                    dev_dict_cut[device] = [options[0], options[5]]
+                else:
+                    dev_dict_cut[device] = [options[0]]
+            case 'Звуковые платы':
+                dev_dict_cut[device] = [options[0]]
+            case 'Адаптеры сетевого интерфейса':
+                dev_dict_cut[device] = [options[0]]
+            case 'Мониторы':
+                dev_dict_cut[device] = [options[1]]
+    return dev_dict_cut
+
+for device, options in get_data_from_file('ZAMFIN.csv').items():
     print(f'{device}: {options}')
+print(listdir())
