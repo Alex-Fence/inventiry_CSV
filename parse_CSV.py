7a7f6621ac3932f6c7225d71d8a02702e30f7b99
Alex-Fence <bokuto@mail.ru>
2024-10-28 17:29:35 +0300

    добавил запись ip адреса

--- parse_CSV.py
+++ parse_CSV.py
@@ -1,7 +1,7 @@
 import csv
 import os
 from os import listdir
-
+import socket
 
 # функция получения данных из файла
 def get_data_from_file(f_name: str) -> dict:
@@ -22,6 +22,9 @@ def get_data_from_file(f_name: str) -> dict:
                         hw_dict[name_s].append(i)
     # переделка ключа Материнская плата из-за его искажения при загрузки
     hw_dict['Материнская плата'] = hw_dict.pop('\ufeffМатеринская плата')
+    for device, options in hw_dict.items():
+        if device == 'Мониторы':
+            print(f'{device}: {len(options)} {options}')
 
     dev_dict_cut = {}  # словарь для сохранения отфильтрованных данных
     # очистка словаря от лишних значений
@@ -50,7 +53,10 @@ def get_data_from_file(f_name: str) -> dict:
             case 'Адаптеры сетевого интерфейса':
                 dev_dict_cut[device] = [options[0]]
             case 'Мониторы':
-                dev_dict_cut[device] = [options[i] for i in range(0, len(options), 5)]
+                dev_dict_cut[device] = []
+                if len(options) > 0:
+                    dev_dict_cut[device] = [[monitor] for monitor in options if not (":" in monitor)]
+                print(dev_dict_cut[device])
     return dev_dict_cut
 
 
@@ -60,6 +66,11 @@ def write_data_to_file(comp_name: str, data: dict):
     with open('comp_data.csv', 'a', newline='', encoding='utf-8') as csv_file:
         csv_file.write('\n')
         csv_file.write(comp_name[:-4] + '\n')
+        try:
+            ip_addr = socket.gethostbyname(comp_name[0:-4])
+            csv_file.write(f'{ip_addr}\n')
+        except socket.gaierror:
+            print(f'Не удалось получить IP адрес компьютера {comp_name}')
         # Записываем данные построчно
         for key, values in data.items():
             if len(values) > 0:
@@ -72,7 +83,10 @@ def write_data_to_file(comp_name: str, data: dict):
                 csv_file.write(f"{key}, НЕТ ДАННЫХ\n")
 
 
-for comp in listdir('./pc_dir'):
-    print(comp)
-    comp_data = get_data_from_file(comp)
-    write_data_to_file(comp, comp_data)
+if __name__ == '__main__':
+    if os.path.exists('comp_data.csv'):
+        os.remove('comp_data.csv')
+    for comp in listdir('./pc_dir'):
+        print(comp)
+        comp_data = get_data_from_file(comp)
+        write_data_to_file(comp, comp_data)
